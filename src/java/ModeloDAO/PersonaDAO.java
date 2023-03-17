43f7b585bb50a48f461a94c51cb94d1cc24745c0
APRENDIZ <APRENDIZ@BOGAPRCGMFSD5XX>
2023-03-17 17:15:27 -0500

    Avance_Gil_nuevamente

--- src/java/ModeloDAO/PersonaDAO.java
+++ src/java/ModeloDAO/PersonaDAO.java
@@ -5,10 +5,172 @@
  */
 package ModeloDAO;
 
+import ModeloVO.PersonaVO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import util.ConexionDB;
+import util.crud;
+
 /**
  *
  * @author xJuanDa
  */
-public class PersonaDAO {
+public class PersonaDAO extends ConexionDB implements crud{
+    private Connection conexion;
+    private PreparedStatement puente;
+    private ResultSet mensajero;
+    private boolean operacion = false;
+    private String sql;
+    private String perDocumento="", perTipoDocumento="", perNombre="", perApellido="", perTelefono="", perDireccion="", perUsuario="", perSede="", perEstado="";
+
+    public PersonaDAO() {}
+    public PersonaDAO(PersonaVO perVO){
+        super();
+        try {
+            conexion = this.obtenerConexion();
+            perDocumento=perVO.getPerDocumento();
+            perTipoDocumento=perVO.getPerTipoDocumento();
+            perNombre=perVO.getPerNombre();
+            perApellido=perVO.getPerApellido();
+            perTelefono=perVO.getPerTelefono();
+            perDireccion=perVO.getPerDireccion();
+            perUsuario=perVO.getPerUsuario();
+            perSede=perVO.getPerSede();
+            perEstado=perVO.getPerEstado();
+        } catch (Exception e) {
+            Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    @Override
+    public boolean agregarRegistro() {
+        try {
+            sql = "insert into tblpersona values (?,?,?,?,?,?,?,?,?);";
+            puente = conexion.prepareStatement(sql);
+            puente.setString(1, perDocumento);
+            puente.setString(2, perTipoDocumento);
+            puente.setString(3, perNombre);
+            puente.setString(4, perApellido);
+            puente.setString(5, perTelefono);
+            puente.setString(6, perDireccion);
+            puente.setString(7, perUsuario);
+            puente.setString(8, perSede);
+            puente.setString(9, perEstado);
+            puente.executeUpdate();
+            operacion = true;
+        } catch (Exception e) {
+            Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            try {
+                this.cerrarConexion();
+            } catch (Exception e) {
+                Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return operacion;
+    }
+
+    @Override
+    public boolean actualizarRegistro() {
+        try {
+            sql = "update tblpersona set PerTipoDocumento=?, PerNombre=?, PerApellido=?, PerTelefono=?, PerDireccion=?, PerUsuario=?, PerSede=?, PerEstado=? where PerDocumento=?;";
+            puente = conexion.prepareStatement(sql);
+            puente.setString(1, perTipoDocumento);
+            puente.setString(2, perNombre);
+            puente.setString(3, perApellido);
+            puente.setString(4, perTelefono);
+            puente.setString(5, perDireccion);
+            puente.setString(6, perUsuario);
+            puente.setString(7, perSede);
+            puente.setString(8, perEstado);
+            puente.setString(9, perDocumento);
+            puente.executeUpdate();
+            operacion = true;
+        } catch (Exception e) {
+            Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            try {
+                this.cerrarConexion();
+            } catch (Exception e) {
+                Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return operacion;
+    }
+
+    @Override
+    public boolean eliminarRegistro() {
+        try {
+            sql = "update tblpersona set PerEstado='Inactivo' where PerDocumento=?;";
+            puente = conexion.prepareStatement(sql);
+            puente.setString(1, perDocumento);
+            puente.executeUpdate();
+            operacion = true;
+        } catch (Exception e) {
+            Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            try {
+                this.cerrarConexion();
+            } catch (Exception e) {
+                Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return operacion;
+    }
     
+    public PersonaVO consultarPorDocumento(String documento){
+        PersonaVO perVO = null;
+
+        try {
+            conexion = this.obtenerConexion();
+            sql = "select * from vehiculo where vehplaca=?;";
+            puente = conexion.prepareStatement(sql);
+            puente.setString(1, documento);
+            mensajero = puente.executeQuery();
+
+            while (mensajero.next()) {
+                perVO = new PersonaVO(documento, mensajero.getString(2), mensajero.getString(3), mensajero.getString(4), mensajero.getString(5), mensajero.getString(6), mensajero.getString(7), mensajero.getString(8), mensajero.getString(9));
+            }
+        } catch (Exception e) {
+            Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            try {
+                this.cerrarConexion();
+            } catch (Exception e) {
+                Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return perVO;
+    }
+    
+    public ArrayList<PersonaVO> listar() {
+        ArrayList<PersonaVO> personaLista = new ArrayList<>();
+        try {
+            conexion = this.obtenerConexion();
+            sql = "select * from tblpersona;";
+            puente = conexion.prepareStatement(sql);
+            mensajero = puente.executeQuery();
+
+            while (mensajero.next()) {
+                PersonaVO perVO = new PersonaVO(mensajero.getString(1), mensajero.getString(2), mensajero.getString(3), mensajero.getString(4), mensajero.getString(5), mensajero.getString(6), mensajero.getString(7), mensajero.getString(8), mensajero.getString(9));
+
+                personaLista.add(perVO);
+
+            }
+
+        } catch (Exception e) {
+            Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            try {
+                this.cerrarConexion();
+            } catch (Exception e) {
+                Logger.getLogger(PersonaDAO.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+        return personaLista;
+    }
 }
